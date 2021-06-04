bea0e8c542da53f7f13a168d4cf23e96a5307106
AaryadevChandra <aaryadevc@gmail.com>
2021-06-04 12:04:18 +0530

    medium

--- ContainerWithMostWaterLeet.py
+++ ContainerWithMostWaterLeet.py
@@ -1,13 +1,14 @@
 # medium question
 import numpy as np
 
-# [[1, 1], [8, 2], [6, 3], [2, 4], [5, 5], [4, 6], [8, 7], [3, 8], [7, 9]]
-# [[12, 1], [3, 2], [7, 3], [9, 4], [2, 5]]
-arr = np.array([[1, 1], [8, 2], [6, 3], [2, 4], [5, 5], [4, 6], [8, 7], [3, 8], [7, 9]])
+arr = np.array([[12, 1], [13, 2], [7, 3], [5, 4], [2, 5], [8, 6], [3, 7], [11, 8], [6, 9]])
 
 area = np.empty(50)
 num = 0
 
+
+temp = 0
+temp = int(temp)
 for x in range(0, len(arr) - 1):
     for y in range(x + 1, len(arr)):
         width = arr[x][1] - arr[y][1]
@@ -17,14 +18,9 @@ for x in range(0, len(arr) - 1):
         else:
             height = arr[x][0]
         
-        area[num] = width * height
-        num = num + 1
-temp = 0
-for x in range(0, len(area)):
-    for y in range(x+1, len(area)):
-        if area[y] < area[x]:
-            temp = area[y]
-            area[y] = area[x]
-            area[x] = temp
+        temp = width * height
+        area = np.append(area, temp)
 
-print(f"The container with the most volume has a volume = {area[-1]}")
+area = np.sort(area)
+
+print(f"The container with the most volume has a volume = {np.amax(area)}")
